8a6171ef875d8e0ad970e4af9551b55352b5b269
Siddhanttade <siddhanttade19@gmail.com>
2025-09-24 22:16:00 +0530

    adding comment,like,playlist,tweet and fixing some bugs

---
+++ src/models/comment.model.js
@@ -0,0 +1,23 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema=new mongoose.Schema(
+    {
+        content:{
+            type:String,
+            required:true,
+        },
+        video:{
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+    },{timestamps:true}
+)
+
+commentSchema.plugin(mongooseAggregatePaginate); //plugin gives pagination feature to aggregate queries
+
+export const Comment=mongoose.model("Comment",commentSchema);
